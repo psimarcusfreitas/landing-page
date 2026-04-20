8270a5257d015afbfb236df2c74dd0109147072e
Marcus <marcus@example.com>
2026-04-20 20:09:15 -0300

    fix: update base path again to /home/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/bruno-rhein-landing-page/",
+  base: "/home/",
   server: {
     host: "::",
     port: 8080,
