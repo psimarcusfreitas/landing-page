a601d307b0424a0d85e86068df82812464397214
Marcus <marcus@example.com>
2026-04-20 18:51:40 -0300

    fix: update base path in vite config for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/landing-page/",
+  base: "/bruno-rhein-landing-page/",
   server: {
     host: "::",
     port: 8080,
